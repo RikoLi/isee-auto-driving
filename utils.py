bf0d4ff52447d3a5ffe7f136d907d37dba2ce53a
rikoli <939958083@qq.com>
2019-05-25 02:33:10 +0800

    Add utils.py

--- utils.py
+++ utils.py
@@ -30,5 +30,63 @@ def batch_resize(width, height, data_folder):
         print(cnt, '/', len(imgs))
         cnt += 1
 
+# 手动标注路标
+def extract_icon(data_path, save_path):
+    datas = glob.glob(data_path+'/*.jpg')
+    num = len(datas)
+    ix = 0
+    iy = 0
+    ex = 0
+    ey = 0
+    i = 0
+    roi = None
+    drawing = False
+    is_next = False
+    img = None
+    def drawROI(event, x, y, flags, param):  # 鼠标监听回调
+        nonlocal ix, iy, ex, ey, drawing, is_next, roi, img # 向上一层嵌套外寻找变量
+        if event == cv.EVENT_LBUTTONDOWN:
+            drawing = True
+            is_next = False
+            print('Top-left corner at (%d, %d)' % (x, y))
+            ix = x
+            iy = y
+        elif event == cv.EVENT_MOUSEMOVE:
+            if drawing:
+                ex = x
+                ey = y
+        elif event == cv.EVENT_LBUTTONUP:
+            drawing = False
+            print('Bottom-right corner at (%d, %d)' % (x, y))
+            ex = x
+            ey = y
+            is_next = True
+            new_img = img.copy()
+            show_img = img.copy()
+            roi = new_img[iy:ey+1, ix:ex+1]
+            cv.rectangle(show_img, (ix,iy), (ex,ey), (0,0,255), 2)
+            cv.imshow('window', show_img)
+
+    cv.namedWindow('window')
+    cv.setMouseCallback('window', drawROI)
+    while True:
+        img = cv.imread(datas[i])
+        cv.imshow('window', img)
+        
+        if i+1 > num:
+            print('---Labeling finished!---')
+            break
+        key = cv.waitKey(0) & 0xFF
+        if key == ord('q'):
+            break
+        elif key == ord('s') and is_next:
+            is_next = False
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+            cv.imwrite(save_path+'/'+str(i+1)+'.jpg', roi)
+            print('Image', str(i+1), 'is done! x:%d, y:%d, w:%d, h:%d' % (ix, iy, ex-ix, ey-iy))
+        i += 1
+
 if __name__ == '__main__':
-    batch_resize(640, 480, 'class_stop')
+    # batch_resize(640, 480, 'class_stop')
+    extract_icon('./class_10', 'class_10_icon')
